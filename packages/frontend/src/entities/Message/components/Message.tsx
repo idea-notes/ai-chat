3d968c3b4b654fc73fa427475492d11b9c8be4dd
Said <gazievs387@gmail.com>
2025-08-15 10:50:07 +0300

    delte comments

--- packages/frontend/src/entities/Message/components/Message.tsx
+++ packages/frontend/src/entities/Message/components/Message.tsx
@@ -15,7 +15,7 @@ function MessageComponent({message} : MessageProps) {
     return (
         <Box 
             sx={(t) => ({
-                bgcolor: t.palette.background.lightGray, // msg color // some text color
+                bgcolor: t.palette.background.lightGray,
                 width: isUserMessage ? "40%" : "60%", 
                 boxSizing: "border-box", 
                 alignSelf: isUserMessage ? "flex-end" : "center", 
