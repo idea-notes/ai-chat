64258c48e81346166e1051eb84d369741fbb55a9
Said <gazievs387@gmail.com>
2025-08-15 10:15:14 +0300

    1

--- packages/frontend/src/entities/Message/components/Message.tsx
+++ packages/frontend/src/entities/Message/components/Message.tsx
@@ -21,7 +21,7 @@ function MessageComponent({message} : MessageProps) {
                 alignSelf: isUserMessage ? "flex-end" : "center", 
                 whiteSpace: "pre-wrap", 
                 wordBreak: "break-word", 
-                borderRadius: 4, // some comments // comments one one 1111
+                borderRadius: 4, // some comments // comments one
                 p: 2, 
                 mx: "2%",
                 my: 2
